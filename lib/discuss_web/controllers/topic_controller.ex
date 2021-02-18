7d658798e89cefb677c9e6fb2382e0ccadb7a697
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-02-18 15:11:48 -0300

    Make Topic creation view

---
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,10 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+end
