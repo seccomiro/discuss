fd2103e92755cf62313bc108059769689b4800d4
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-02-19 00:40:28 -0300

    Make insert Topic or show errors

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -5,6 +5,15 @@ defmodule DiscussWeb.TopicController do
 
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Discuss.Repo.insert(changeset) do
+      {:ok, topic} -> IO.inspect(topic)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
