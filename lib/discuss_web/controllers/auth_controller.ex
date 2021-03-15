d8c3e668d88f9f949a2de44b9345afeb168b4f79
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-15 20:35:24 -0300

    Implement better auth flow

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -2,6 +2,42 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
+  alias Discuss.User
+  alias Discuss.Repo
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: "github"
+    }
+
+    changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:info, "Error signing in")
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+
+      user ->
+        {:ok, user}
+    end
   end
 end
