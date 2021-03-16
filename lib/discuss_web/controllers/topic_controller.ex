a0bd0ebc22bc2eca8ab630d09953cc7605dd505b
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-03-16 14:04:52 -0300

    Make topics#create attach user

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -17,7 +17,11 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    IO.inspect(conn.assigns.user)
+    changeset =
+      conn.assigns.user
+      |> Ecto.build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
